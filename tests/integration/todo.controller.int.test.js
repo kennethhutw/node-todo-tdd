329b3920f3c3dcacbd1e367a10c7cbff925654ba
Hu Chia Wei <kenneth_hu@livemail.tw>
2023-05-16 14:37:20 +0800

    finish Get ToDo test

--- tests/integration/todo.controller.int.test.js
+++ tests/integration/todo.controller.int.test.js
@@ -4,7 +4,20 @@ const newTodo = require("../mock-data/new-todo.json")
 
 const endpointUrl = '/todos/';
 
+let firstTodo;
+
 describe(endpointUrl,()=>{
+    it('GET ' + endpointUrl, async () => {
+      const response = await request(app).get(endpointUrl);
+      expect(response.statusCode).toBe(200);
+      expect(Array.isArray(response.body)).toBeTruthy();
+        expect(response.body[0].title).toBeDefined();
+        expect(response.body[0].done).toBeDefined();
+        firstTodo = response.body[0];
+    })
+
+
+
     it('POST ' + endpointUrl, async () => {
         const response = await request(app)
           .post(endpointUrl)
@@ -13,5 +26,16 @@ describe(endpointUrl,()=>{
         expect(response.body.title).toBe(newTodo.title);
         expect(response.body.done).toBe(newTodo.done);
       });
-      
+
+      it('should return error 500 on malformed data with POST ' + endpointUrl, async () => {
+        const response = await request(app)
+        .post(endpointUrl).send({title:"Missing done property"});
+
+        expect(response.statusCode).toBe(500);
+        expect(response.body).toStrictEqual({
+          message:"Todo validation failed: done: Path `done` is required."
+        })
+      })
+
+
 })
